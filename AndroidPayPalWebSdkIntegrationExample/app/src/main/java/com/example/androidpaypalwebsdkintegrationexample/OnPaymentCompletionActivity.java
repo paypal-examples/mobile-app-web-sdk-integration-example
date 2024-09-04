c037313946d9ef7abc2acb874da4e54145aa351e
Ravi Shekhar <ravshekhar@paypal.com>
2024-09-04 18:56:35 -0500

    Add android example

---
+++ AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/OnPaymentCompletionActivity.java
@@ -0,0 +1,54 @@
+package com.example.androidpaypalwebsdkintegrationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.androidpaypalwebsdkintegrationexample.databinding.ActivityOnPaymentCompletionBinding;
+
+import org.json.JSONObject;
+
+/**
+ * This is a sample activity which demonstrates an example payment success activity page.
+ * This Activity should be invoked after the buyer has reviewed and approved their payment
+ * on web application hosted by merchant.
+ * The web application should use payment methods like PayPal, Braintree to collect payments from buyer
+ */
+public class OnPaymentCompletionActivity extends AppCompatActivity {
+    private ActivityOnPaymentCompletionBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_payment_completion);
+        binding = ActivityOnPaymentCompletionBinding.inflate(getLayoutInflater());
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Uri uri = getIntent().getData();
+        String payPalOrderID = null;
+
+        if (uri != null) {
+            payPalOrderID = uri.getQueryParameter("payPalOrderID");
+            Log.i("OnPayment", "onStart payPalOrderID = " + payPalOrderID);
+        } else {
+            Log.i("OnPayment", "onStart No payPalOrderID Found");
+        }
+
+        TextView textView = (TextView) findViewById(R.id.payPalOrderId);
+        textView.setText(payPalOrderID);
+
+        // Fetch the PayPal Order Details from your REST APIs. The REST APIs should only exposes minimal data as required by your APP.
+        // You should also capture or authorize the amounts before fulfilling the buyer order.
+    }
+}
---
+++ AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/ShoppingCartFragment.java
@@ -0,0 +1,91 @@
+package com.example.androidpaypalwebsdkintegrationexample;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.androidpaypalwebsdkintegrationexample.databinding.FragmentShoppingCartBinding;
+
+/**
+ * This Fragment demonstrates a sample shopping cart fragment which should be rendered after buyer
+ * has added items to their cart and clicks shopping cart button.
+ * This view should also render a list of selected payment methods like PayPal and link / button to
+ * view full list of all available payment options.
+ *
+ * This example demonstrates two buttons,
+ * 1. Render a PayPal Web SDK integration and Orders API using a Chrome Custom Tabs
+ * 2. Render a PayPal Checkout integration using Orders API.
+ *
+ * The buttons rendered on this page takes the buyers to a web application hosted by merchant which should
+ * lists down payment methods available, e.g. PayPal.
+ *
+ * When the buyer clicks the PayPal button inside the Chrome Custom tab, they would be refirected
+ * to PayPal.com to review and approve their payment.
+ * After they approve the payment on PayPal.com, they would be taken back to the web application
+ * hosted by the merchant.
+ *
+ * The web application needs to render a redirect button which should take the buyers back to this
+ * Android App using App Links/Deep Links.
+ */
+public class ShoppingCartFragment extends Fragment {
+
+    private FragmentShoppingCartBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentShoppingCartBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Uri payPalSpbUrl = Uri.parse(getString(R.string.merchant_hosted_checkout_web_url_spb));
+        Uri payPalApiCheckoutUrl = Uri.parse(getString(R.string.merchant_hosted_checkout_web_url_api));
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+
+        binding.checkoutButtonPaypalSpb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customTabsIntent.launchUrl(binding.getRoot().getContext(), payPalSpbUrl);
+            }
+        });
+        binding.checkoutButtonPaypalApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customTabsIntent.launchUrl(binding.getRoot().getContext(), payPalApiCheckoutUrl);
+            }
+        });
+        binding.openPaypalDotComButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customTabsIntent.launchUrl(binding.getRoot().getContext(), Uri.parse(getString(R.string.paypal_dot_com_web_url)));
+            }
+        });
+        binding.productCatalogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ShoppingCartFragment.this)
+                        .navigate(R.id.action_ShoppingCartFragment_to_CatalogFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
