dd75e30266f1c9a957bcc57323fb14f8a6581576
Ravi Shekhar <ravishekhar@users.noreply.github.com>
2024-09-11 16:31:18 -0500

    Android Example with Custom Tabs  (#3)
    * Add android example
    
    * Fix cancel url
    
    * Update AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/CatalogFragment.java
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>
    
    * Update AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/CatalogFragment.java
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>
    
    * Update AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/ShoppingCartFragment.java
    
    Co-authored-by: Tim Chow <timothydchow@gmail.com>
    
    * Update AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/ShoppingCartFragment.java
    
    Co-authored-by: Tim Chow <timothydchow@gmail.com>
    
    * remove auto generated codes and extraneous super invocation
    
    * Use liner layout instead of constrained layout and remove PayPal.com button
    
    * Remove wrapping of single widgets inside layouts, use linear layout instead of app bar
    
    * Use const for urls
    
    ---------
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>
    Co-authored-by: Tim Chow <timothydchow@gmail.com>

---
+++ AndroidPayPalWebSdkIntegrationExample/app/src/main/java/com/example/androidpaypalwebsdkintegrationexample/CatalogFragment.java
@@ -0,0 +1,35 @@
+package com.example.androidpaypalwebsdkintegrationexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.androidpaypalwebsdkintegrationexample.databinding.FragmentCatalogBinding;
+
+/**
+ * This Fragment is a sample fragment to demonstrate a view to host the fully Catalog of products
+ * the merchant's android app may be offering.
+ */
+public class CatalogFragment extends Fragment {
+
+    private FragmentCatalogBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentCatalogBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
